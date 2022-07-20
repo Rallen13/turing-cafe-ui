a47c0764c8b78ed22c1cef3663b8f057404cac84
Rachel Allen <rallenco13@gmail.com>
2022-07-20 10:02:44 -0600

    completed App.js setup and have complete reservations displaying

--- src/App/App.js
+++ src/App/App.js
@@ -45,7 +45,14 @@ class App extends Component {
         <div className="reservation-form"></div>
         <section className="reservations-container">
           {this.state.reservations.map((reservation) => {
-            return <h2 key={reservation.id}>{reservation.name}</h2>;
+            return (
+              <div key={reservation.id} className="reservation-card">
+                <h2>{reservation.name}</h2>
+                <h2> {reservation.date} </h2>
+                <h2> {reservation.time} </h2>
+                <h2> {reservation.number} </h2>
+              </div>
+            );
           })}
         </section>
       </div>
